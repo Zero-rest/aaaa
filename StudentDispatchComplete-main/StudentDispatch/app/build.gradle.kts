3f5290ff9856a083a2721d3ff8068b7eac8de43c
Zero-rest <46242415+Zero-rest@users.noreply.github.com>
2026-02-13 03:51:51 +0330

    Update build.gradle.kts

--- StudentDispatchComplete-main/StudentDispatch/app/build.gradle.kts
+++ StudentDispatchComplete-main/StudentDispatch/app/build.gradle.kts
@@ -38,18 +38,23 @@ android {
 }
 
 dependencies {
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+
     implementation("androidx.activity:activity-compose:1.9.1")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.compose.material3:material3")
+  
+    implementation("androidx.compose.material3:material3-window-size-class")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
 
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
